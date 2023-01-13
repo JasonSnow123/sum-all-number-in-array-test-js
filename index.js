08d1c67369528e92cc39f1463251656d66234e16
JasonSnow123 <m.jason.2018@gmail.com>
2023-01-12 19:29:54 -0500

    created recursive function to nested arrays

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      sum += sumItems(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
+};
 
 module.exports = sumItems;
